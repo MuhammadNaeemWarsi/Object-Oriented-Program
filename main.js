17f47083c4ad9fc03e538844f990302df79409f5
Muhammad Naeem Warsi <161728783+MuhammadNaeemWarsi@users.noreply.github.com>
2024-05-19 19:16:28 +0500

    Object Oriented Program project completed

---
+++ main.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+let persons = new Person();
+let programStart = async (persons) => {
+    do {
+        console.log("welcome");
+        let ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "whom would you like to interact with?",
+            choices: ["staff", "student", "exit"]
+        });
+        if (ans.select == "staff") {
+            console.log("you approach the staff room.Please feelfree to ask");
+        }
+        else if (ans.select == "student") {
+            let ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the student name"
+            });
+            let student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                let name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello i am ${name.name}.Nice to meet you.`);
+                console.log("New student added");
+                console.log("Current student list");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello i am ${Student.name}.Nice to see you`);
+                console.log("Existing student list");
+                console.log(persons.students);
+            }
+        }
+        else if (ans.select == "exit") {
+            console.log("Exiting the program...");
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
